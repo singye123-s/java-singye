b6bac4f77531e1d905874b490a75743efe13722c
singye123-s <chaaand123@gmail.com>
2026-05-31 20:06:57 -0700

    hhhhhh

---
+++ nturalnumbers.java
@@ -0,0 +1,20 @@
+public class nturalnumbers {
+    public static void main(String[] args){
+        for(int i = 1; i<=10;i++){
+            System.out.println(i);
+        
+        }
+    int i = 10;
+    while(i>=0){
+        System.out.println(i);
+        i--;
+
+    }
+    
+    
+    
+    
+    }
+
+     
+}
