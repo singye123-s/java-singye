7e021cc9bf942579c38ce8755d01d1ddf6543f67
singye123-s <chaaand123@gmail.com>
2026-06-14 20:01:52 -0700

    njj

---
+++ Employee.java
@@ -0,0 +1,28 @@
+
+public class Employee {
+    int empno;
+    String name;
+    float sal;
+    Employee(){
+        System.out.println("*********");
+        empno = 101;
+        name ="aashish";
+        sal = 5000f;
+    }
+    void displayDetails(){
+        System.out.println(empno + "|" + name + "|" + sal);
+
+    }
+}
+class Main{
+
+    public static void main(String[] args){
+        Employee emp1 = new Employee();
+        Employee emp2 = new Employee();
+        Employee emp3 = new Employee();
+
+        emp1.displayDetails();
+        emp2.displayDetails();
+        emp3.displayDetails();
+    }
+}
