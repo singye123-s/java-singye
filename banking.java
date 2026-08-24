dc7d61397b205ce4bb86b0d7b38ec88c9e94fee2
singye123-s <chaaand123@gmail.com>
2026-08-23 19:27:04 -0700

    dbf

---
+++ banking.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+
+
+
+public class banking {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+       try {
+              
+              System.out.println("What is your bank id ( letters)");
+              String name = sc.nextLine();
+              if ( name.equals("AOPEICG")){
+                throw new Exception("Your bank subscription has ran out");
+              } 
+              else{
+                System.out.println(" welcome to your funds");
+              }
+              
+
+              
+            
+
+
+        } 
+        
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+}
+}
